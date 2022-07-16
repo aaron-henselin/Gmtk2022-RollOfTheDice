895ca990761bc843b9b9fddab7c9b8abfcd45abe
Aaron Henselin <aaron.henselin@catalystfire.com>
2022-07-15 21:39:05 -0400

    asdf

---
+++ RollTheDiceGmtk2022/Game/BlazorTimer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Timers;
+
+namespace RollTheDiceGmtk2022.Game
+{
+
+    public class BlazorTimer
+    {
+        private Timer _timer;
+
+        public void SetTimer(double interval)
+        {
+            _timer = new Timer(interval);
+            _timer.AutoReset = true;
+            _timer.Elapsed += NotifyTimerElapsed;
+            _timer.Enabled = true;
+        }
+
+        public event Action OnElapsed;
+
+        private void NotifyTimerElapsed(Object source, ElapsedEventArgs e)
+        {
+            OnElapsed?.Invoke();
+        }
+    }
+}
