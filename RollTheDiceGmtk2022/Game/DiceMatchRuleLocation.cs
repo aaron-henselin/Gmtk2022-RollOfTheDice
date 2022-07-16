dfeb711a0619833375e9e3ed4e72f13d4aec398b
Brian Pegram <btpegram@gmail.com>
2022-07-15 22:28:43 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ RollTheDiceGmtk2022/Game/DiceMatchRuleLocation.cs
@@ -0,0 +1,18 @@
+﻿namespace RollTheDiceGmtk2022.Game
+{
+    public struct DiceMatchRuleLocation
+    {
+        public DiceMatchRuleLocation(bool inDashboard, int collectionIndex = 0, int slotIndex = 0)
+        {
+            InDashboard = inDashboard;
+            CollectionIndex = collectionIndex;
+            SlotIndex = slotIndex;
+        }
+
+        public bool InDashboard { get; set; }
+
+        public int CollectionIndex { get; set; }
+
+        public int SlotIndex { get; set; }
+    }
+}
