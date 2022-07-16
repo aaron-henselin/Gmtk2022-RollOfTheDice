bf4a8cf0c8922d241d6a9c72e3da649995a2cd59
Brian Pegram <btpegram@gmail.com>
2022-07-16 01:39:01 -0400

    asdf

---
+++ RollTheDiceGmtk2022/Game/CardLocation.cs
@@ -0,0 +1,15 @@
+﻿namespace RollTheDiceGmtk2022.Game
+{
+    public struct CardLocation
+    {
+        public CardLocation(bool inHand, int collectionIndex = 0)
+        {
+            InHand = inHand;
+            CollectionIndex = collectionIndex;
+        }
+
+        public bool InHand { get; set; }
+
+        public int CollectionIndex { get; set; }
+    }
+}
